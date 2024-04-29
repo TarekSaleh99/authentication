e0d7f6d446f3b05feffd6f29d034175c58bf696d
Tarek Saleh <135518833+TarekSaleh99@users.noreply.github.com>
2024-04-29 15:23:00 +0200

    Commit #1

---
+++ authfunc/views.py
@@ -0,0 +1,45 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login,logout, authenticate
+from .forms import SignupForm, LoginForm
+
+# Create your views here.
+
+# Home page
+def index(request):
+    return render(request, 'index.html')
+
+
+# Signup page
+def user_signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm
+    return render(request, 'signup.html', {'form':form})
+
+
+# Login page
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('index')
+    else:
+        form = LoginForm()
+    return render(request, 'login.html', {'form':form})
+
+def user_logout(request):
+    logout(request)
+    return redirect('login')
+
+
+def user_profile(request):
+    return render(request, 'profile.html')
